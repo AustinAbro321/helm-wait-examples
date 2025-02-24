18b47c626b414e52e9e9e261c0011fae86e2bc71
Austin Abro <AustinAbro321@gmail.com>
2025-02-24 16:37:39 +0000

    basic watcher example
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+set -euo pipefail # fail if any of the waits fail
+kubectl delete ns legacy --ignore-not-found=true
+kubectl delete ns watcher --ignore-not-found=true
 helm_path='../helm/bin/helm'
-## basic example
-kubectl delete ns legacy
-kubectl delete ns watcher
-$helm_path install basic-legacy basic/ --wait -n legacy --create-namespace
-kubectl wait --for=condition=available deployment/basic-legacy-podinfo -n legacy --timeout=5s
 
-$helm_path install basic-watcher basic/ --wait=legacy -n watcher --create-namespace
+## basic example
+$helm_path install basic-legacy basic/ --wait -n legacy --create-namespace
+kubectl wait --for=condition=available deployment/basic-legacy-podinfo -n legacy --timeout=1s
+
+$helm_path install basic-watcher basic/ --wait=watcher -n watcher --create-namespace
 kubectl wait --for=condition=available deployment/basic-watcher-podinfo -n watcher --timeout=1s
